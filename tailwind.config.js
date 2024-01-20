e5c41798f7d83ba1ef0504915922780300463a6d
Wiraa <ur.wiraananda@gmail.com>
2024-01-20 16:58:23 +0800

    fixing: memperbaiki masalah

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> 646df248d543bfe3b4b26e8608c74bf6a5ba0bd2
-=======
->>>>>>> main
   ],
   theme: {
     extend: {
