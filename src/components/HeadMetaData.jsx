6944c86e80e17df47727548e4b7ed9a18ba2934b
Wiraa <ur.wiraananda@gmail.com>
2024-01-22 17:28:15 +0800

    refactor: menerapkan dry dengan component seo

---
+++ src/components/HeadMetaData.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export const HeadMetaData = () => {
+  return (
+    <Head>
+      <title>Netflix Clone</title>
+      <meta
+        name="keywords"
+        content="Netflix Clone, Netflix, IMDB Clone, TMDB Clone, Website Film, Sinopsis Film, Film"
+      />
+      <meta
+        name="description"
+        content="Tempat dimana semua karya nyata terpampang"
+      />
+    </Hea>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,10 @@
 import HomePage from "@/components/HomePage";
-import Head from "next/head";
+import { HeadMetaData } from "@/components/HeadMetaData";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Netflix Clone</title>
-        <meta
-          name="keywords"
-          content="Netflix Clone, Netflix, IMDB Clone, TMDB Clone, Website Film, Sinopsis Film, Film"
-        />
-        <meta
-          name="description"
-          content="Tempat dimana semua karya nyata terpampang"
-        />
-      </Head>
+      <HeadMetaData />
       <main>
         <section className="w-[100vw] h-[100vh]">
           <HomePage />
