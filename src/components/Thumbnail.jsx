f2ccc56218c5e398847b35f923b6a93d62937827
Wiraa <ur.wiraananda@gmail.com>
2024-01-23 19:58:16 +0800

    refactor: memperbaiki ui

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -4,19 +4,18 @@ import Link from "next/link";
 export default function Thumbnail({ data, title }) {
   return (
     <article>
-      <h1 className="font-bold text-[2rem]">{title}</h1>
-      <div className="flex space-x-[.5rem] overflow-x-auto">
+      <h1 className="font-bold text-2xl">{title}</h1>
+      <div className="flex">
         {data?.map((thumbnail) => (
-          <Link
-            className="w-[9rem] sm:w-[10.5rem] md:w-[12rem]"
-            href={`/movies/${thumbnail.id}`}
-            key={thumbnail.id}
-          >
-            <img
-              key={thumbnail.id}
-              src={`${imgUrl}${thumbnail.poster_path}`}
-              alt="Poster Movie"
-            />
+          <Link href={`/movies/${thumbnail.id}`} key={thumbnail.id}>
+            <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+              <img
+                className="w-full h-auto"
+                key={thumbnail.id}
+                src={`${imgUrl}${thumbnail.backdrop_path}`}
+                alt="Poster Movie"
+              />
+            </div>
           </Link>
         ))}
       </div>
